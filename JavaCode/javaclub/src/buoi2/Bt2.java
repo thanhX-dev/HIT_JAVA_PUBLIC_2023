b3c961e164523a5bed88ed14429c279053c188b3
thanhX-dev <thanhlasobonthegioi@gmail.com>
2024-07-22 22:12:32 +0700

    buoi2

--- JavaCode/javaclub/src/buoi2/Bt2.java
+++ JavaCode/javaclub/src/buoi2/Bt2.java
@@ -1,7 +1,36 @@
 package buoi2;
 
+import java.util.Scanner;
+
 public class Bt2 {
    public static void main(String[] args) {
-	System.out.println("helo");
+	Scanner sc = new Scanner (System.in);
+	System.out.println("nhap so phan tu: ");
+	int n = sc.nextInt();
+	
+	int a[]= new int[n];
+	System.out.println("nhap mang: ");
+	for (int i=0 ; i<n ;i++) {
+		a[i] = sc.nextInt();
+	}
+	int amax = -99999;
+	int amin = 99999;
+	for (int i=0 ; i<n ;i++) {
+		if(amax < a[i])
+			amax =a[i];
+	}
+	for (int i=0 ; i<n ;i++) {
+		if(amin > a[i]) {
+			amin = a[i];
+		}
+	}
+	int s = amax - amin;
+	System.out.println("mang da nhap :");
+	for (int i=0 ; i<n ;i++) {
+		System.out.print(a[i] + " ");
+	}
+	System.out.println();
+	System.out.println("phan tu lon nhat: " + amax + '\n' + "kc lon nhat giua cac phan tu: " + s);
+	sc.close();
 }
 }
